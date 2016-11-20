15f503c8ab548109f4268049f0bfe3f53277725c
Théo Poizat <theo.poizat@etu.unistra.fr>
2016-11-20 19:49:41 +0100

    Hash des chunks par 1Mo (todo : utilisable comme une librairie, vérifier que les chunks sont corrects)

--- hash-file.c
+++ hash-file.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <gcrypt.h>
 
-// renvoie la taille du fichier fd en octet
+/* renvoie la taille du fichier FILE * fd en octet
+*/
 long tailleFichier(FILE * fd)
 {
   long taille;
@@ -18,32 +19,28 @@ long tailleFichier(FILE * fd)
   return taille;
 }
 
-// renvoie le hash d'un fichier
+/* renvoie le hash d'un fichier
+  char * fichier = chemin du fichier
+*/
 char * hashFichier(char * fichier)
 {
-  // RECUPERATION DU FICHIER
-
   FILE * fd = fopen(fichier, "r");
 
   long taille = tailleFichier(fd);
 
-  void * buffer = malloc(taille);
+  void * buffer = malloc(taille); // buffer pour stocker le fichier entier
 
   fread(buffer, 1, taille, fd); // buffer contient maintenant les données du fichier
 
   // CALCUL DU HASH
-
-  unsigned char hash[32]; // hash calculé (taille fixe car algo. fixe)
+  unsigned char hash[32]; // hash calculé
 
   gcry_md_hash_buffer(GCRY_MD_SHA256, hash, buffer, taille);
 
   // AFFICHAGE DU HASH
-
   char * sortie = malloc(sizeof(char) * ((32 * 2) + 1)), * p = sortie;
 
-  int i;
-
-  for(i = 0; i < 32; i++, p += 2)
+  for(int i = 0; i < 32; i++, p += 2) // hash converti
   {
     snprintf(p, 3, "%02x", hash[i]);
   }
@@ -53,7 +50,58 @@ char * hashFichier(char * fichier)
   return sortie;
 }
 
+char ** hashChunks(char * fichier)
+{
+  FILE * fd = fopen(fichier, "r");
+
+  long taille = tailleFichier(fd);
+
+  void * buffer = malloc(1000000); // buffer pour stocker le fichier entier
+
+  int nChunks = taille / 1000000 + 1;
+
+  unsigned char ** hashs = malloc(nChunks * sizeof(unsigned char *)); // tableau des hashs calculés
+  char ** sorties = malloc(nChunks * sizeof(char *)); // tableau des hashs convertis
+  char ** p = malloc(nChunks * sizeof(char *)); // tableau hashs convertis (seconde référence pour l'affichage)
+
+  for(int i = 0; i < nChunks; i++)
+  {
+    hashs[i] = malloc(32 * sizeof(unsigned char));
+    sorties[i] = malloc(sizeof(char) * ((32 * 2) + 1));
+    p[i] = sorties[i];
+  }
+
+  // boucle
+  for(int k = 0; k < nChunks; k++)
+  {
+    fread(buffer, 1, 1000000, fd); // buffer contient maintenant les données du chunk
+
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashs[k], buffer, 1000000); // calcul du hash
+
+    for(int j = 0; j < 32; j++, p[k] += 2) // conversion du hash
+    {
+      snprintf(p[k], 3, "%02x", hashs[k][j]);
+    }
+  }
+
+  fclose(fd);
+
+  for(int a = 0; a < nChunks; a++)
+  {
+    printf("CHUNK %d : %s\n", a, sorties[a]);
+  }
+
+  return sorties;
+}
+
 int main(int argc, char *argv[])
 {
-  hashFichier(argv[1]);
+  if(argc != 2)
+  {
+    exit(1);
+  }
+
+  printf("%s : %s\n", argv[1], hashFichier(argv[1]));
+
+  hashChunks(argv[1]);
 }
