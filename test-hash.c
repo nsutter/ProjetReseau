9724024986020dae6ee35af2fc190d528dbfe6aa
nsutter <suttercyril.sutternicolas@gmail.com>
2016-12-12 22:26:58 +0100

    fichier de test pour les hashs

---
+++ test-hash.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gcrypt.h>
+#include "hash-file.h"
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+  {
+    exit(1);
+  }
+
+  printf("%s : %s", argv[1], hashFichier(argv[1]);
+
+  int nb;
+
+  char res ** hashAllChunks(argv[1], &nb);
+
+  for(int i = 0; i < nb; i++)
+  {
+    printf("CHUNK %d : %s\n", i, res[i]);
+  }
+}
